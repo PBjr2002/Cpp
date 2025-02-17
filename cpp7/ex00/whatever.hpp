d7b887b33f05c79ff43c1b24617fd00f421edd22
Paulo <paulobernardo.2002@gmail.com>
2025-02-17 14:48:02 +0000

    cpp7 ex00 done

---
+++ cpp7/ex00/whatever.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pauberna <pauberna@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-02-12 12:19:32 by pauberna          #+#    #+#             */
+/*   Updated: 2025-02-12 12:19:32 by pauberna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+# include <string>
+# include <cstring>
+# include <math.h>
+# include <cmath>
+# include <fstream>
+
+# define RESET "\033[0m"
+# define GREEN "\033[1m\033[32m"
+# define RED "\033[1m\033[31m"
+# define YELLOW "\033[1m\033[33m"
+
+template<typename T>
+void	swap(T &a, T &b)
+{
+	T	&tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+T	min(T &a, T &b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+template<typename T>
+T	max(T &a, T &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+#endif
